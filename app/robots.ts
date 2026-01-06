669e038f6748449bd17ca4b9cc8a85cf129b0ab7
prettykaur <pkaur.dev@gmail.com>
2026-01-06 16:56:03 +0800

    Set up robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,15 @@
 // app/robots.ts
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://tryverba.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/search', '/search?', '/*?q='],
+      },
+    ],
+    sitemap: 'https://tryverba.com/sitemap.xml',
   };
 }
